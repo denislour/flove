e6373f1d34dd9b0b198d49662e8084e6f1aa17c4
Lektor <denislour@gmail.com>
2022-06-18 21:56:43 +0700

    New action-button components with slyteling.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@
 <script>
 import MainNav from "@/components/MainNav.vue";
 
+// Need to update playground js
+// Checking test resource
+// Learning with carefuly
+
 export default {
   name: "App",
   components: {
