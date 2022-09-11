3a012da41954b22cec7120c5289eaa5fb2bb6fbf
Lektor <denislour@gmail.com>
2022-09-11 16:20:29 +0700

    Update Hero component and isolating all of components

---
+++ src/components/JobSearch/HeroSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <section class="flex flex-col h-screen pt-10 pb-20">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1"></div>
+        <div class="col-start-2 col-span-5">
+          <head-line></head-line>
+          <job-search-form></job-search-form>
+        </div>
+        <div class="col-start-7 col-span-5 self-center justify-self-center">
+          <img
+            class="w-80 h-80 object-contain"
+            src="https://images.pexels.com/photos/3205567/pexels-photo-3205567.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+        </div>
+        <div class="col-start-12 col-span-1"></div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import HeadLine from "@/components/JobSearch/HeadLine.vue";
+import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+
+export default {
+  name: "HeroSection",
+  components: {
+    HeadLine,
+    JobSearchForm,
+  },
+};
+</script>
---
+++ src/components/JobSearch/JobSearchForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form
+    class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+    <div class="flex flex-nowrap flex-1 h-full text-base font-light">
+      <div class="relative flex items-center flex-1 h-full pr-3">
+        <div class="absolute left-0 -top-10">Role</div>
+        <input
+          type="text"
+          v-model="role"
+          placeholder="Software engineer"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+      <span
+        class="flex items-center h-full px-3 border-l border-r border-brand-gray-3"
+      >
+        in
+      </span>
+      <div class="relative flex items-center flex-1 h-full pr-3">
+        <div class="absolute left-0 -top-10">Where?</div>
+        <input
+          type="text"
+          v-model="location"
+          placeholder="Los Angeles"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+      <action-button
+        text="Search"
+        type="secondary"
+        class="rounded-r-3xl"
+      ></action-button>
+    </div>
+  </form>
+</template>
+
+<script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+export default {
+  name: "JobSearchForm",
+  components: { ActionButton },
+  data() {
+    return {
+      role: "",
+      location: "",
+    };
+  },
+};
+</script>
